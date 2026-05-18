8fab461c67fb201c0f1a1dfe75ffb1cb08a4d12c
Daniel Jensen <dan2533@byu.edu>
2026-05-18 13:01:41 -0600

    Initial Commit

---
+++ PromptMAXX/PromptMAXXApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptMAXXApp.swift
+//  PromptMAXX
+//
+//  Created by Daniel Jensen on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct PromptMAXXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
